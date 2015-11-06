4307afdc65105c698cb77766f4e60757e0b1305f
Raja Khan <getrk@yahoo.com>
2015-11-06 17:43:47 -0500

    Update keychain.js
    JavaScript plugin for keychain manipulation

--- www/keychain.js
+++ www/keychain.js
@@ -15,4 +15,12 @@ var iosKeychain = {
                  function(err){onError(err)},
                  'Keychain', 'get', [ 'empty' ]);
     },
+    
+    remove: function(key, onSuccess, onError) {
+        cordova.exec(
+                 key,
+                 function(data){onSuccess(data)},
+                 function(err){onError(err)},
+                 'Keychain', 'remove', [ 'empty' ]);
+    },    
 };
