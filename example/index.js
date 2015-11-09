bf73f78dd94e35b96b82d5f2643576620de62d5e
Raja Khan <getrk@yahoo.com>
2015-11-09 12:17:43 -0500

    Create index.js

---
+++ example/index.js
@@ -0,0 +1,40 @@
+function onBodyLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+ 
+function onDeviceReady() {
+    try {
+ 
+    } catch (e) {
+        debug.error(e);
+    }
+}
+
+var onSuccess = function(value) {
+    alert("GET SUCCESS - Key: " + key + " Value: " + value);
+};
+var onError = function(error) {
+     alert("GET FAIL - Key: " + key + " Error: " + error);
+};
+		
+function doGet() {
+    var chain = new iosKeychain();
+    var key = document.getElementById("get1").value;
+    chain.get(key, onSuccess, onError);
+}
+
+function doSet() {
+    var chain = new iosKeychain();
+
+    var key = document.getElementById("set1").value;
+    var value = document.getElementById("set2").value;
+
+    chain.set(key, value, onSuccess, onError);
+}
+
+function doRemove() {
+    var chain = new iosKeychain();
+    var key = document.getElementById("rem1").value;
+
+    chain.removeForKey(key, onSuccess, onError);
+}
